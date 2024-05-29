d62882eeac0408622d2dc9c5e09889f08312ed6f
jisupark1992 <jisupark1992@gmail.com>
2024-05-29 16:54:28 +0900

    요세푸스 문제 7_2_15

---
+++ src/main/java/org/codingtest/queue/QueueMain.java
@@ -0,0 +1,10 @@
+package org.codingtest.queue;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        System.out.println("--------------------큐 7_2_15 요세푸스 문제 Start--------------------");
+        int N = 5, K = 2;
+        System.out.println("결과 : " + 요세푸스_문제.solution(N, K));
+        System.out.println("--------------------큐 7_2_15 요세푸스 문제 End--------------------");
+    }
+}
