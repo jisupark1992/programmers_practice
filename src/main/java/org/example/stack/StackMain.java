bbf20e19d7c992eba2f82bb5960e771b96d43dc2
jisupark1992 <jisupark1992@gmail.com>
2024-05-29 14:07:11 +0900

    짝지어 제거하기 6_4_11

--- src/main/java/org/example/stack/StackMain.java
+++ src/main/java/org/example/stack/StackMain.java
@@ -12,11 +12,15 @@ public class StackMain {
         int decimal = 123456789;
         System.out.println("입력 값 : " + decimal + ", 결과 : " + 진수_변환하기.solution(decimal));
         System.out.println("--------------------스택 6_3_9 10진수를 2진수로 변환하기 End--------------------");
-        System.out.println("--------------------스택 6_4_10 괄호_회전하기 Start--------------------");
+        System.out.println("--------------------스택 6_4_10 괄호 회전하기 Start--------------------");
         s = "[](){}";
         System.out.println("입력 값 : " + s + ", 결과 : " + 괄호_회전하기.solution(s));
         s = "}]()[{";
         System.out.println("입력 값 : " + s + ", 결과 : " + 괄호_회전하기.solution(s));
-        System.out.println("--------------------스택 6_4_10 괄호_회전하기 End--------------------");
+        System.out.println("--------------------스택 6_4_10 괄호 회전하기 End--------------------");
+        System.out.println("--------------------스택 6_4_11 짝지어 제거하기 Start--------------------");
+        s = "baabaa";
+        System.out.println("입력 값 : " + s + ", 결과 : " + 짝지어_제거하기.solution(s));
+        System.out.println("--------------------스택 6_4_11 짝지어 제거하기 End--------------------");
     }
 }
