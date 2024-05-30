93bbeaa1b1ad4538870a63aee7edbf852a15450b
jisupark1992 <jisupark1992@gmail.com>
2024-05-30 10:54:20 +0900

    카드 뭉치 7_3_17

--- src/main/java/org/codingtest/queue/QueueMain.java
+++ src/main/java/org/codingtest/queue/QueueMain.java
@@ -12,5 +12,11 @@ public class QueueMain {
         int[] progresses = {93, 30, 55}, speeds = {1, 30, 5};
         System.out.println("결과 : " + Arrays.toString(기능_개발.solution(progresses, speeds)));
         System.out.println("--------------------큐 7_3_16 기능 개발 End--------------------");
+        System.out.println("--------------------큐 7_3_17 카드 뭉치 Start--------------------");
+        String[] card1 = {"i", "water", "drink"}, card2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+        System.out.println("card1 : " + Arrays.toString(card1) + ", card2 : " + Arrays.toString(card2) + ", goal: " + Arrays.toString(goal));
+        System.out.println("결과 : " + 카드_뭉치.solution(card1, card2, goal));
+        System.out.println("--------------------큐 7_3_17 카드 뭉치 End--------------------");
     }
 }
