ac89963858e1de3367b0f1adb208c4a1057b4817
jisupark1992 <jisupark1992@gmail.com>
2024-06-02 13:16:38 +0900

    완주하지 못한 선수 8_4_19

--- src/main/java/org/codingtest/hash/HashMain.java
+++ src/main/java/org/codingtest/hash/HashMain.java
@@ -6,6 +6,11 @@ public class HashMain {
         int[] arr = {1, 2, 3, 4, 8};
         int target = 6;
         System.out.println("결과 : " + 두_개의_수로_특정값_만들기.solution(arr, target));
-        System.out.println("--------------------해시 8_4_18 두 개의 수로 특정값 만들기--------------------");
+        System.out.println("--------------------해시 8_4_18 두 개의 수로 특정값 만들기 End--------------------");
+        System.out.println("--------------------해시 8_4_19 완주하지 못한 선수 Start--------------------");
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        System.out.println("결과 : " + 완주하지_못한_선수.solution(participant, completion));
+        System.out.println("--------------------해시 8_4_19 완주하지 못한 선수 End--------------------");
     }
 }
