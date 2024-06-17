9fb4419058b0f63b7b7628485e7ffe212ec5c3c9
jisupark1992 <JisuPark1992@gmail.com>
2024-06-17 09:28:58 +0900

    폰켓몬 10_4_31

--- src/main/java/org/codingtest/set/SetMain.java
+++ src/main/java/org/codingtest/set/SetMain.java
@@ -8,5 +8,9 @@ public class SetMain {
         int[][] operations = {{0, 0, 1}, {0, 1, 2}, {1, 1, 2}};
         System.out.println("결과 : " + Arrays.toString(간단한_유니온_파인드_알고리즘_구현하기.solution(3, operations)));
         System.out.println("--------------------집합 10_3_30 간단한 유니온-파인드 알고리즘 구현하기 End----------------------");
+        System.out.println("--------------------집합 10_4_31 폰켓몬 Start--------------------");
+        int[] nums = {3, 1, 2, 3};
+        System.out.println("결과 : " + 폰켓몬.solution(nums));
+        System.out.println("--------------------집합 10_4_31 폰켓몬 End----------------------");
     }
 }
