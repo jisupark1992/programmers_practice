3d914f1ff2bd42a01fe86ad994b4df5ec688288b
jisupark1992 <JisuPark1992@gmail.com>
2024-06-19 09:31:53 +0900

    섬 연결하기 10_4_33 메인 수정

--- src/main/java/org/codingtest/set/SetMain.java
+++ src/main/java/org/codingtest/set/SetMain.java
@@ -13,9 +13,13 @@ public class SetMain {
         System.out.println("결과 : " + 폰켓몬.solution(nums));
         System.out.println("--------------------집합 10_4_31 폰켓몬 End----------------------");
         System.out.println("--------------------집합 10_4_32 영어 끝말잇기 Start--------------------");
-        int n = 3;
+        int n = 3;`
         String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
         System.out.println("결과 : " + Arrays.toString(영어_끝말잇기.solution(n, words)));
         System.out.println("--------------------집합 10_4_32 영어 끝말잇기 End----------------------");
+        System.out.println("--------------------집합 10_4_33 섬 연결하기 Start--------------------");
+        int[][] costs = {{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}};
+        System.out.println("결과 : " + 섬_연결하기.solution(4, costs));
+        System.out.println("--------------------집합 10_4_33 섬 연결하기 End----------------------");
     }
 }
