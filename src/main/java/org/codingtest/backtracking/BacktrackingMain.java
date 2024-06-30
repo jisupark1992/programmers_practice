4274e3b270d78ff0f40fbf537b5f990aeb7e5dba
jisupark1992 <jisupark1992@gmail.com>
2024-06-30 23:06:17 +0900

    1부터 N까지 숫자 중 합이 10이 되는 조합 구하기 메인 함수 수정

--- src/main/java/org/codingtest/backtracking/BacktrackingMain.java
+++ src/main/java/org/codingtest/backtracking/BacktrackingMain.java
@@ -1,9 +1,12 @@
 package org.codingtest.backtracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BacktrackingMain {
     public static void main(String[] args) {
         System.out.println("--------------------백트래킹 12_2_43 1부터 N까지 숫자 중 합이 10이 되는 조합 구하기 Start--------------------");
-        System.out.println("결과 : ");
+        System.out.println("결과 : " + Arrays.deepToString(new ArrayList[]{백트레킹_몸풀기_문제.solution(5)}));
         System.out.println("--------------------백트래킹 12_2_43 1부터 N까지 숫자 중 합이 10이 되는 조합 구하기 End----------------------");
     }
 }
