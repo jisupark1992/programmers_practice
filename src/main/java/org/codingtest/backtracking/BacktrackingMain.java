15b2f6868dab18e1609ce7e44c24f9f79cd5ad53
jisupark1992 <jisupark1992@gmail.com>
2024-06-30 22:54:45 +0900

    백트래킹 패키지 및 메인 함수 생성

---
+++ src/main/java/org/codingtest/backtracking/BacktrackingMain.java
@@ -0,0 +1,9 @@
+package org.codingtest.backtracking;
+
+public class BacktrackingMain {
+    public static void main(String[] args) {
+        System.out.println("--------------------백트래킹 12_2_43 1부터 N까지 숫자 중 합이 10이 되는 조합 구하기 Start--------------------");
+        System.out.println("결과 : ");
+        System.out.println("--------------------백트래킹 12_2_43 1부터 N까지 숫자 중 합이 10이 되는 조합 구하기 End----------------------");
+    }
+}
