772a5c54a12b0ddc374e37c54b3f09b7ffe57329
jisupark1992 <jisupark1992@gmail.com>
2024-07-06 22:45:32 +0900

    Sort main 함수 수정(13_2_50 계수 정렬 구현하기 추가)

--- src/main/java/org/codingtest/sort/SortMain.java
+++ src/main/java/org/codingtest/sort/SortMain.java
@@ -3,7 +3,8 @@ package org.codingtest.sort;
 public class SortMain {
     public static void main(String[] args) {
         System.out.println("--------------------정렬 13_2_50 계수 정렬 구현하기 Start--------------------");
-        System.out.println("결과 : ");
+        String s = "hello";
+        System.out.println("결과 : " + 계수_정렬_구현하기.solution(s));
         System.out.println("--------------------정렬 13_2_50 계수 정렬 구현하기 End----------------------");
     }
 }
