def4b2a92a5a2babafb28b38c98ddadff377f07e
jisupark1992 <jisupark1992@gmail.com>
2024-07-06 22:43:48 +0900

    Sort package 생성 및 main 실행 함수 생성

---
+++ src/main/java/org/codingtest/sort/SortMain.java
@@ -0,0 +1,9 @@
+package org.codingtest.sort;
+
+public class SortMain {
+    public static void main(String[] args) {
+        System.out.println("--------------------정렬 13_2_50 계수 정렬 구현하기 Start--------------------");
+        System.out.println("결과 : ");
+        System.out.println("--------------------정렬 13_2_50 계수 정렬 구현하기 End----------------------");
+    }
+}
