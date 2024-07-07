c60c06de5f4942a89c0df65ccac8b6fb40be8b92
jisupark1992 <jisupark1992@gmail.com>
2024-07-07 19:47:19 +0900

    Sort main 함수 수정(13_3_53 정수 내림차순으로 배치하기 추가)

--- src/main/java/org/codingtest/sort/SortMain.java
+++ src/main/java/org/codingtest/sort/SortMain.java
@@ -17,5 +17,9 @@ public class SortMain {
         int n = 1;
         System.out.println("결과 : " + Arrays.toString(문자열_내_마음대로_정렬하기.solution(strings, n)));
         System.out.println("--------------------정렬 13_3_52 문자열 내 마음대로 정렬하기 End----------------------");
+        System.out.println("--------------------정렬 13_3_53 정수 내림차순으로 배치하기 Start--------------------");
+        n = 118372;
+        System.out.println("결과 : " + 정수_내림차순으로_배치하기.solution(n));
+        System.out.println("--------------------정렬 13_3_53 정수 내림차순으로 배치하기 End----------------------");
     }
 }
