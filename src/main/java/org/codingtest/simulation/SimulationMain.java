d0438227ed5f91d61fcf4aeab77ea7be47f5926a
jisupark1992 <jisupark1992@gmail.com>
2024-07-13 23:38:43 +0900

    Simulation main 함수 수정(14_2_60 두 행렬을 곱한 후 전치 행렬 만들기 추가)

--- src/main/java/org/codingtest/simulation/SimulationMain.java
+++ src/main/java/org/codingtest/simulation/SimulationMain.java
@@ -9,5 +9,10 @@ public class SimulationMain {
         int n = 1;
         System.out.println("결과 : " + Arrays.deepToString(배열_회전하기.solution(arr, n)));
         System.out.println("--------------------시뮬레이션 14_2_59 배열 회전하기 End----------------------");
+        System.out.println("--------------------시뮬레이션 14_2_60 두 행렬을 곱한 후 전치 행렬 만들기 Start--------------------");
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+        System.out.println("결과 : " + Arrays.deepToString(두_행렬을_곱한_후_전치_행렬_만들기.solution(matrix1, matrix2)));
+        System.out.println("--------------------시뮬레이션 14_2_60 두 행렬을 곱한 후 전치 행렬 만들기 End----------------------");
     }
 }
