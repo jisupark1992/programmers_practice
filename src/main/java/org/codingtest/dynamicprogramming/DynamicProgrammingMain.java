33e72d2f4bcc6ab65a61febe9ea4366da57ec715
jisupark1992 <JisuPark1992@gmail.com>
2024-07-22 09:56:06 +0900

    Dynamic Programming main 함수 수정(15_2_68 LIS 길이 계산하기 추가)

--- src/main/java/org/codingtest/dynamicprogramming/DynamicProgrammingMain.java
+++ src/main/java/org/codingtest/dynamicprogramming/DynamicProgrammingMain.java
@@ -6,5 +6,9 @@ public class DynamicProgrammingMain {
         String str1 = "ABCBDAB", str2 = "BDCAB";
         System.out.println("결과 : " + LCS길이_계산하기.solution(str1, str2));
         System.out.println("--------------------동적 계획법 15_2_67 LCS 길이 계산하기 End----------------------");
+        System.out.println("--------------------동적 계획법 15_2_68 LIS 길이 계산하기 Start--------------------");
+        int[] nums = {1, 4, 2, 3, 1, 5, 7, 3};
+        System.out.println("결과 : " + LIS길이_계산하기.solution(nums));
+        System.out.println("--------------------동적 계획법 15_2_68 LIS 길이 계산하기 End----------------------");
     }
 }
