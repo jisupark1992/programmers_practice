7c27c05b5ccb9200092451361cabbe60f9c8577b
jisupark1992 <JisuPark1992@gmail.com>
2024-07-22 09:41:48 +0900

    Dynamic Programming main 함수 수정(15_2_67 LCS 길이 계산하기 추가)

--- src/main/java/org/codingtest/dynamicprogramming/DynamicProgrammingMain.java
+++ src/main/java/org/codingtest/dynamicprogramming/DynamicProgrammingMain.java
@@ -2,8 +2,9 @@ package org.codingtest.dynamicprogramming;
 
 public class DynamicProgrammingMain {
     public static void main(String[] args) {
-        System.out.println("--------------------동적 계획법 15 Start--------------------");
-        System.out.println("결과 : ");
-        System.out.println("--------------------동적 계획법 15 End----------------------");
+        System.out.println("--------------------동적 계획법 15_2_67 LCS 길이 계산하기 Start--------------------");
+        String str1 = "ABCBDAB", str2 = "BDCAB";
+        System.out.println("결과 : " + LCS길이_계산하기.solution(str1, str2));
+        System.out.println("--------------------동적 계획법 15_2_67 LCS 길이 계산하기 End----------------------");
     }
 }
