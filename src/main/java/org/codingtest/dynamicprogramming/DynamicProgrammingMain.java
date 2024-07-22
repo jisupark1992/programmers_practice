3a5237746107f5bce5a844c6c71490a5b96d00dd
jisupark1992 <JisuPark1992@gmail.com>
2024-07-22 09:25:21 +0900

    동적 계획법 패키지 및 메인 클래스 추가

---
+++ src/main/java/org/codingtest/dynamicprogramming/DynamicProgrammingMain.java
@@ -0,0 +1,9 @@
+package org.codingtest.dynamicprogramming;
+
+public class DynamicProgrammingMain {
+    public static void main(String[] args) {
+        System.out.println("--------------------동적 계획법 15 Start--------------------");
+        System.out.println("결과 : ");
+        System.out.println("--------------------동적 계획법 15 End----------------------");
+    }
+}
